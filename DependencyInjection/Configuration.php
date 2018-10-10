e8f90ed6f8cfe5450ade48577a319bf7c5563b1d
dev1-ads <dev1@adstrategy.fr>
2018-10-10 17:34:54 +0200

    firstcomit

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kinulab\SonataGentellelaThemeBundle\DependencyInjection;
+namespace Carlead\SonataThemeBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('kinulab_sonata_gentellela_theme');
+        $rootNode = $treeBuilder->root('carlead_sonata_theme_bundle');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
